00f167ed11427704afce734f28a1a4b164254839
Shreyas <insind938@IN.ZOOMCOBRA.COM>
2020-12-08 20:34:01 +0530

    Orders URL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-dff12/us-central1/api" //API (cloud function url)
+  baseURL: "https://us-central1-clone-dff12.cloudfunctions.net/api" //API (cloud function url)
 });
 
 export default instance;
+
+//Prod
+//https://us-central1-clone-dff12.cloudfunctions.net/api
+
+//Local
+//http://localhost:5001/clone-dff12/us-central1/api
